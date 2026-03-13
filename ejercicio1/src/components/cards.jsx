69727602f0ed9fc4c2167813668356652e94487d
Alfaro294 <jenniferalfaro0819@gmail.com>
2026-03-13 15:40:23 -0600

    Modificaciones

--- ejercicio1/src/components/cards.jsx
+++ ejercicio1/src/components/cards.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import "./Card.css";
 
-const Cards = ({ text }) => {
+const Cards = ({titulo, texto}) => {
   return (
     <div class="card" style="width: 18rem;">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">{titulo}</h5>
         <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
+          {texto}
         </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
       </div>
     </div>
   );
